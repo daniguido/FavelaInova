5372ae6a02c46f1bbd635be12c2da7b79a671e46
daniguido <106547411+daniguido@users.noreply.github.com>
2022-06-29 01:38:41 -0300

    alteraçao menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
+/*function selectNav(navlink){
+    var navitem  = document.getElementById("navitem");
+    navitem.style.borderBottomColor = "#f38a12"; 
+    navitem.style.borderBottomStyle= "solid";
+    navitem.style.borderBottomWidth = "10px";
 
+    navlink.setAttribute("disabled", "true");
+    setTimeout(clearDemo, 2000, navlink);
+
+
+  }
+  function doDemo (button) {
+    var square = document.getElementById("square");
+    square.style.backgroundColor = "#fa4";
+    button.setAttribute("disabled", "true");
+    setTimeout(clearDemo, 2000, button);
+  }
+  
+ function desmarcaNav(navitem){
+    var navbarnav = document.getElementById("navbarnav");
+    navbarnav.style.borderBottomColor = "#transparent";
+    navitem.removeAttribute("disabled");
+
+
+  }*/

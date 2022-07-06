6c1c98e98d87e9cd4cf360b199093188d3cfe785
daniguido <106547411+daniguido@users.noreply.github.com>
2022-07-06 04:35:34 -0300

    alteraçao
    pagina Imprensa coluna e leia mais e leia menos com css e js
    Cores padroes e font padrao
     add de pasta imagens2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,105 @@
-/*function selectNav(navlink){
-    var navitem  = document.getElementById("navitem");
-    navitem.style.borderBottomColor = "#f38a12"; 
-    navitem.style.borderBottomStyle= "solid";
-    navitem.style.borderBottomWidth = "10px";
+/* const txt= document.querySelector(".card-text");
+const txtInitialHeight = txt.clientHeight;
+const overflow = document.querySelector(".overflow");
+const btn = document.querySelector(".toggle");
 
-    navlink.setAttribute("disabled", "true");
-    setTimeout(clearDemo, 2000, navlink);
+btn.addEventListener("click", initToggle);
+
+function initToggle(e){
+  txt.style.maxHeight = 
+  e.target.dataset.state ==="mais"
+  ? '${txt.scrollHeight}%'
+  : '${txtInitialHeight}%';
+
+  e.target.setAttribute(
+    "data-state",
+    e.target.dataset.state ==="mais" ? "menos" : "mais"
+  );
+
+  e.target.innerHTML = 
+  e.target.dataset.state === "mais" ? "Leia mais" : "Leia menos";
+
+  overflow.setAttribute(
+    "data-state",
+    e.target.dataset.state ==="mais" ? "visible" : "hidden"
+  );
+} */
 
 
+/* 
+document.querySelectorAll('.post-body').forEach(function (el) {
+   
+  var fullText = el.querySelector('.full-text');
+  var shortText =  el.querySelector('.short-text');
+  
+  if (! shortText) return;
+  
+  el.querySelector('.read-more').addEventListener('click', function () {
+     fullText.style.display = '';
+     shortText.style.display = 'none';
+  })
+  
+  el.querySelector('.read-less').addEventListener('click', function () {
+     fullText.style.display = 'none';
+     shortText.style.display = '';
+  })
+})
+
+
+function readMoreRome() { //finds function
+  var dots = document.getElementById("dots"); //returns element that has the ID attribute with value, searches for dots
+  var moreText = document.getElementById("more"); // '' '' searches for more
+  var btnText = document.getElementById("myBtn"); // '' '' searches for myBtn
+  
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
   }
-  function doDemo (button) {
-    var square = document.getElementById("square");
-    square.style.backgroundColor = "#fa4";
-    button.setAttribute("disabled", "true");
-    setTimeout(clearDemo, 2000, button);
   }
   
- function desmarcaNav(navitem){
-    var navbarnav = document.getElementById("navbarnav");
-    navbarnav.style.borderBottomColor = "#transparent";
-    navitem.removeAttribute("disabled");
+  function readMoreBuda() { //finds function
+  var dots = document.getElementById("dots2"); //returns element that has the ID attribute with value
+  var moreText = document.getElementById("more2"); // '' '' searches for more2
+  var btnText = document.getElementById("myBtn2"); // '' '' searches for myBtn2
+  
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
+  }
+  } [
+*/
 
 
-  }*/
+  /****    FUNCIONOU 1 *******/
+/*   function mostra(id){
+    if (document.getElementById(id).style.display =='none'){
+     document.getElementById(id).style.display = 'block';
+    }else {document.getElementById(id).style.display ='none';} 
+   } */
+    
+      
+function vermais(){
+  let pontos = document.getElementById("pontos");
+  let maisTexto = document.getElementById("mais");
+  let btnVermais = document.getElementById("btnVerMais");
+
+  if (pontos.style.display ==="none"){
+    pontos.style.display="inline";
+    maisTexto.style.display="none";
+    btnVermais.innerHTML="Ver mais";
+  }else{
+    pontos.style.display="none";
+    maisTexto.style.display="inline";
+    btnVermais.innerHTML="Ver menos"
+  }
+}
